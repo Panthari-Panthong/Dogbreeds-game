70261acf01701fedca9c26164139ac04f15c9c19
Panthari <YOUR@EMAIL.com>
2019-10-10 12:24:32 +0200

    fixed doglist action

--- src/actions/dogslist.js
+++ src/actions/dogslist.js
@@ -1,25 +1,26 @@
-import * as request from 'superagent';
-export const SET_DOGSLIST = 'SET_DOGSLIST';
-export function setDogsList(breeds) {
+import * as request from 'superagent'
+export const SET_DOGSLIST = 'SET_DOGSLIST'
+
+export function setDogslist(breeds) {
   return {
     type: 'SET_DOGSLIST',
     payload: breeds
-  };
+  }
 }
 
-export function getDogsList() {
-  return function(dispatch, getState) {
-    const currentBreeds = getState().dogList;
-    if (currentBreeds.length !== 0) {
-      return;
-    }
 
+export function getDogslist() {
+  return function (dispatch, getState) {
+    const currentBreeds = getState().breeds
+
+    if (currentBreeds.length !== 0) {
+      return
+    }
 
 
     request('https://dog.ceo/api/breeds/list/all')
       .then(response => {
         dispatch(setDogslist(Object.keys(response.body.message)))
-
       })
   }
 }
