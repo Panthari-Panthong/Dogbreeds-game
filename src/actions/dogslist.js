e28176faefdc273e785be2b9a4fbfd75c15b6ee3
stijnblokker <stijnblokker@gmail.com>
2019-10-10 11:23:42 +0200

    file controlling

--- src/actions/dogslist.js
+++ src/actions/dogslist.js
@@ -1,26 +1,21 @@
-import * as request from 'superagent'
-export const SET_DOGSLIST = 'SET_DOGSLIST'
-
-export function setDogslist(breeds) {
+import * as request from 'superagent';
+export const SET_DOGSLIST = 'SET_DOGSLIST';
+export function setDogsList(breeds) {
   return {
     type: 'SET_DOGSLIST',
     payload: breeds
-  }
+  };
 }
 
-
-export function getDogslist() {
-  return function (dispatch, getState) {
-    const currentBreeds = getState().breeds
-
+export function getDogsList() {
+  return function(dispatch, getState) {
+    const currentBreeds = getState().dogList;
     if (currentBreeds.length !== 0) {
-      return
+      return;
     }
 
-
-    request('https://dog.ceo/api/breeds/list/all')
-      .then(response => {
-        dispatch(setDogslist(Object.keys(response.body.message)))
-      })
-  }
+    request('https://dog.ceo/api/breeds/list/all').then(response => {
+      dispatch(setDogsList(Object.keys(response.body.message)));
+    });
+  };
 }
