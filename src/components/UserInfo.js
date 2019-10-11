f964c1f79382776be6561c3854a731d5ad5fd01e
dung-phan <madewithcrayons@gmail.com>
2019-10-11 09:59:15 +0200

    add css files

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,20 +1,17 @@
-import React from 'react'
-
-
+import React from 'react';
+import './UserInfo.css';
 export default class UserInfo extends React.Component {
-
   render() {
-    const user = this.props.user
+    const user = this.props.user;
     return (
       <div>
         <ul>
-          <li>User name: {user.name}</li>
-          <li>Score: {user.score}</li>
-          <li>Total questions: {user.totalQuestions}</li>
-          <li>Streak: {user.streak}</li>
+          <li className='user-info'>User name: {user.name}</li>
+          <li className='user-info'>Score: {user.score}</li>
+          <li className='user-info'>Total questions: {user.totalQuestions}</li>
+          <li className='user-info'>Streak: {user.streak}</li>
         </ul>
       </div>
-    )
+    );
   }
 }
-
