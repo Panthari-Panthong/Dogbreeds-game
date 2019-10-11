9ca336013f3e52599bd2aee72336c082e01812c0
stijnblokker <stijnblokker@gmail.com>
2019-10-11 09:57:37 +0200

    progress bass basic, not finished

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -5,6 +5,16 @@ export default class UserInfo extends React.Component {
 
   render() {
     const user = this.props.user
+    const { score, totalQuestions } = user
+    //  console.log(typeof(totalQuestions);
+    const total = (totalQuestions == 0) ? 0 :
+      console.log(total);
+    console.log(score / total);
+    let progres = (user.score / user.totalQuestions) * 100
+    // console.log(progres);
+
+
+
     return (
       <div>
         <ul>
@@ -12,6 +22,7 @@ export default class UserInfo extends React.Component {
           <li>Score: {user.score}</li>
           <li>Total questions: {user.totalQuestions}</li>
           <li>Streak: {user.streak}</li>
+          <li>Progres: {progres} %</li>
         </ul>
       </div>
     )
